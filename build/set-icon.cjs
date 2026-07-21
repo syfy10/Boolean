30cfe43c53659168914b27bed44980c31822ccbd
saz3 Labs <saz3labs@gmail.com>
2026-07-21 16:33:19 -0400

    Release Boolean 0.9.41

--- build/set-icon.cjs
+++ build/set-icon.cjs
@@ -11,8 +11,8 @@ rcedit(process.argv[2], {
     LegalCopyright: "Copyright 2026 Boolean",
     OriginalFilename: "Boolean-core.exe"
   },
-  "file-version": "0.9.40",
-  "product-version": "0.9.40"
+  "file-version": "0.9.41",
+  "product-version": "0.9.41"
 })
   .then(() => console.log("icon + version info set"))
   .catch((err) => {
